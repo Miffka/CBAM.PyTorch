ecc9d5b9b6608fa8cf8774be50d276d821a4d6ee
Miffka <miffka1992@inbox.com>
2021-03-29 21:55:15 +0300

    [setup] Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="cbam",
+    packages=find_packages(),
+    version="0.1.0",
+    description="Unofficial CBAM implementation",
+    author="luuuyi, developer0hye, ice-tong",
+    license="",
+)
